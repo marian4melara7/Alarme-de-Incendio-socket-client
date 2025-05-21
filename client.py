d1fa24289e2fdd7816e051dc543233a0e8e770be
Mariana M Melara <mariana.melara@icloud.com>
2025-05-20 21:50:40 -0300

    V. 0

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8080))
+
+    while True:
+        message = input("Client: ")
+        client_socket.send(message.encode('utf-8'))
+        if message.lower() == 'exit':
+            print("Connection closed by the client.")
+            break
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    start_client()
